1257b585e0ae07299e578899a82e1312e177ab29
Nitin Mohapatra <nitinmohapatra26@gmail.com>
2025-10-04 10:30:28 +0530

    Changes Done

---
+++ code.py
@@ -0,0 +1,10 @@
+arr = [0,1,2,2,3,0,4,2]
+target = 2
+j = 0
+
+for i in range(len(arr)):
+    if arr[i] != target:
+        arr[j] = arr[i]
+        j+=1
+    
+print(arr[:j])
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+j = arr.index(2)
